294628bb39b24f1000bfd19303443d7e8151975b
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2018-11-17 17:37:39 +0100

    nav improv

--- awesome-project/public/javascripts/script.js
+++ awesome-project/public/javascripts/script.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
+// function myFunction() {
+//   document.getElementById("fa-sort-up").style.display = "block";
+// }
+document.getElementById("a").onmouseover = function() {
+  document.getElementById("b").style.display = "flex";
+};
