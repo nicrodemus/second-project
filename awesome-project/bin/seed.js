5d3d1cf61872a4167143df2ab9da7000607766ba
Nicolas Pe <perrot.nic@gmail.com>
2018-11-20 13:31:46 +0100

    seed add

--- awesome-project/bin/seed.js
+++ awesome-project/bin/seed.js
@@ -157,24 +157,148 @@ const productData = [
     color: ["noir", "vert"]
   },
   {
-    productName: "Wichita",
-    pictures: [
-      "../images/product-images/homme/sautoire/1/IMG_4027.JPG",
-      "../images/product-images/homme/sautoire/1/IMG_4028.JPG",
-      "../images/product-images/homme/sautoire/1/IMG_4029.JPG"
-    ],
-    typeOfProduct: "sautoir",
-    price: "110 €",
-    // collection: "apache",
-    stones: [
-      { name: ["Turquoise", "Onyx"] },
-      {
-        img: ["../images/amethyste.png", "../images/jade.png"]
-      }
-    ],
-    gender: "male",
-    color: ["noir", "vert"]
-  }
+    productName: "Apache",
+   pictures: [
+     "../images/product-images/homme/sautoire/2/IMG_4030.JPG",
+     "../images/product-images/homme/sautoire/2/IMG_4032.JPG",
+     "../images/product-images/homme/sautoire/2/IMG_4033.JPG"
+   ],
+   typeOfProduct: "sautoir",
+   price: "125 €",
+   // collection: "apache",
+   stones: [
+     { name: [ "Turquoise","Onyx", "Cornaline"]},
+     {
+       img: ["../images/stones-pictures/cornaline.png", "../images/stones-pictures/onyx.png","../images/stones-pictures/turquoise.png", ]
+     }
+   ],
+   gender: "male",
+   color: ["noir","bleu","rouge"]
+ },
+ 
+ {
+  productName: "Stonehenge",
+ pictures: [
+   "../images/product-images/homme/sautoire/3/IMG_4034.JPG",
+   "../images/product-images/homme/sautoire/3/IMG_4035.JPG",
+   "../images/product-images/homme/sautoire/3/IMG_4036.JPG"
+ ],
+ typeOfProduct: "sautoir",
+ price: "100 €",
+ // collection: "apache",
+ stones: [
+   { name: [ "Jaspe","Onyx"]},
+   {
+     img: ["../images/stones-pictures/jaspe.png", "../images/stones-pictures/onyx.png", ]
+   }
+ ],
+ gender: "male",
+ color: ["noir","vert"]
+},
+
+ // -------------------------FEMME--------------- BRACELET
+ {
+  productName: "Etna",
+ pictures: [
+   "../images/product-images/homme/bracelet/1/IMG_4091.JPG",
+   "../images/product-images/homme/bracelet/1/IMG_4092.JPG",
+   "../images/product-images/homme/bracelet/1/IMG_4093.JPG"
+ ],
+ typeOfProduct: "bracelet",
+ price: "40 €",
+
+ stones: [
+   { name: ["Onyx", "Pierre de Lave"] },
+   {
+     img: ["../images/stones-pictures/onyx.png","../images/stones-pictures/pierre-de-lave.png"]
+   }
+ ],
+ gender: "female",
+ color: ["noir",]
+},
+
+
+{
+  productName: "Springfield",
+ pictures: [
+   "../images/product-images/homme/bracelet/2/IMG_4091.JPG",
+   "../images/product-images/homme/bracelet/2/IMG_4092.JPG",
+   "../images/product-images/homme/bracelet/2/IMG_4093.JPG"
+ ],
+ typeOfProduct: "bracelet",
+ price: "40 €",
+
+ stones: [
+   { name: ["Jaspe", "Turquoise"] },
+   {
+     img: ["../images/stones-pictures/jaspe.png", "../images/stones-pictures/turquoise.png"]
+   }
+ ],
+ gender: "female",
+ color: ["beige","bleu"]
+},
+
+{
+  productName: "Indian Summer",
+ pictures: [
+   "../images/product-images/homme/bracelet/3/IMG_4097.JPG",
+   "../images/product-images/homme/bracelet/3/IMG_4098.JPG",
+   "../images/product-images/homme/bracelet/3/IMG_4100.JPG"
+ ],
+ typeOfProduct: "bracelet",
+ price: "50 €",
+
+ stones: [
+   { name: ["Cornaline", "Aigue Marine"] },
+   {
+     img: ["../images/stones-pictures/cornaline.png", "../images/stones-pictures/aigue-marine.png"]
+   }
+ ],
+ gender: "female",
+ color: ["rouge","bleu"]
+},
+
+
+{
+  productName: "Venice Beach",
+ pictures: [
+   "../images/product-images/homme/bracelet/4/IMG_4101.JPG",
+   "../images/product-images/homme/bracelet/4/IMG_4103.JPG",
+   "../images/product-images/homme/bracelet/4/IMG_4104.JPG"
+ ],
+ typeOfProduct: "bracelet",
+ price: "40 €",
+
+ stones: [
+   { name: ["Corail", "Turquoise"] },
+   {
+     img: ["../images/stones-pictures/corail.png", "../images/stones-pictures/turquoise.png"]
+   }
+ ],
+ gender: "female",
+ color: ["rouge","bleu"]
+},
+
+
+{
+  productName: "Venice Beach",
+ pictures: [
+   "../images/product-images/homme/bracelet/6/IMG_4105.JPG",
+   "../images/product-images/homme/bracelet/6/IMG_4106.JPG",
+   "../images/product-images/homme/bracelet/6/IMG_4107.JPG"
+ ],
+ typeOfProduct: "bracelet",
+ price: "40 €",
+
+ stones: [
+   { name: ["Jaspe", "Lapis Lazuli"] },
+   {
+     img: ["../images/stones-pictures/jaspe.png", "../images/stones-pictures/lapis_lazuli.png"]
+   }
+ ],
+ gender: "female",
+ color: ["beige","bleu"]
+},
 ];
 
 Product.create(productData)
